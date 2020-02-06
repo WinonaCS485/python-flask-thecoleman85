599ec64a9301237502379741ab831e9ae4159e46
Cole Walsh <cole.t.walsh@gmail.com>
2020-02-06 12:22:10 -0600

    Update app.py
    http://127.0.0.1:8520/

--- app.py
+++ app.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/cakes')
-def cakes():
-    return 'Yummy cakes!'
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0',port='8520')
